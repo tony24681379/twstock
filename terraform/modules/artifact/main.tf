791a2a8594bec079711d275c06c18e3c227f713e
Tony Huang <yellow24681379@gmail.com>
2026-02-17 22:17:16 +0800

    feat: add google_artifact_registry_repository rule

--- terraform/modules/artifact/main.tf
+++ terraform/modules/artifact/main.tf
@@ -10,4 +10,23 @@ resource "google_artifact_registry_repository" "docker" {
     app = "twstock"
     env = var.environment
   }
+
+  cleanup_policy_dry_run = false
+
+  cleanup_policies {
+    id     = "keep-latest-only"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 1
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
+    condition {
+      tag_state  = "UNTAGGED"
+      older_than = "86400s"
+    }
+  }
 }
